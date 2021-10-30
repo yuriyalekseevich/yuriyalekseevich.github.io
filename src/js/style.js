aa6bdccc64e7abf4099f978eb7f2e604b926bef1
yuriyalekseevich <88712447+yuriyalekseevich@users.noreply.github.com>
2021-10-30 18:14:26 +0300

    Add files via upload

---
+++ src/js/style.js
@@ -0,0 +1,17 @@
+function onEntry(entry){
+  entry.forEach(change =>{
+    if (change.isIntersecting){
+     change.target.classList.add('element-show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.about-text2, .about-text3, .what_can1, .anim, #pr1, .stat-te, .rev1, .con-text1');
+
+for (let elm of elements){
+  observer.observe(elm);
+}
+
